8837afa61eae4a5554fb706d6fc51230a987f4e6
kushiny2003 <kushiny2003@gmail.com>
2026-09-07 20:44:19 +0530

    assignment questions

--- Arrays.py
+++ Arrays.py
@@ -1,1 +1,73 @@
-print("kushiiiiimmmmm")
+a=10
+b=20
+sum=(a+b)
+print(sum)
+
+
+2)
+n=8
+if n % 2==0:
+    print("enter even number")
+else:
+    print("enter odd nunber")
+ 
+3)
+n=-2
+if n > 0:
+    print("positive")
+elif n < 0:
+    print("negative")
+else: 
+    print("invalid")
+
+
+6)
+age=22
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")    
+
+
+
+10)
+for i in range(1,51):
+    print(i)
+
+
+
+11)
+for i in range(1,101):
+    print(i)
+
+
+4)
+num1=[20,40,56,39]
+num2=[1,2,3]
+
+if num1 > num2:
+    print("num1 is larger")
+else:
+    print("num2 is larger")
+
+
+5)
+num1=[2,3,4,5]
+num2=[20,30,40,50]
+num3=[100,300,400]
+
+if num1 > num2:
+    print("large")
+elif num2 > num3:
+    print("small")
+elif num1 == num2:
+    print("equal")
+else:
+    print("invalid")   
+
+
+
+
+9)
+for i in range(1,101):
+    print(i)
